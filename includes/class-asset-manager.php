1c5b5cae167ed0eb80ce91fdd0f97b7c0dd4b8a4
subas-roy <subasroy46@gmail.com>
2026-03-03 18:24:00 +0600

    load built-in jquery

--- includes/class-asset-manager.php
+++ includes/class-asset-manager.php
@@ -11,7 +11,7 @@ class Asset_Manager
   {
     wp_enqueue_style('asset-management-css', PLUGIN_ASSETS . 'css/style.css', [], AM_VERSION);
     wp_enqueue_script('asset-management-js', PLUGIN_ASSETS . 'js/script.js', [], AM_VERSION, true);
-    wp_enqueue_script('asset-management2-js', PLUGIN_ASSETS . 'js/script2.js', [], AM_VERSION, true);
-    wp_enqueue_script('asset-management-jQuery', '//cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js', [], AM_VERSION, true);
+    wp_enqueue_script('asset-management2-js', PLUGIN_ASSETS . 'js/script2.js', ['jquery'], AM_VERSION, true);
+    // wp_enqueue_script('asset-management-jQuery', '//cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js', [], AM_VERSION, true);
   }
 }
